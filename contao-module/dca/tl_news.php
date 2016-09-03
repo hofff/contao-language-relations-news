88c5f1ed0fdd7c8a86da2e5d89cb0e4f028b322b
Oliver Hoff <oliver@hofff.com>
2016-09-03 21:09:12 +0200

    dont use use-statements in dca

--- contao-module/dca/tl_news.php
+++ contao-module/dca/tl_news.php
@@ -1,28 +1,25 @@
 <?php
 
-use Hofff\Contao\LanguageRelations\DCA\RelationsDCABuilder;
-use Hofff\Contao\LanguageRelations\DCA\RelationsDCABuilderConfig;
-use Hofff\Contao\LanguageRelations\Relations;
-use Hofff\Contao\Selectri\Model\Tree\SQLAdjacencyTreeDataFactory;
-
 call_user_func(function() {
-	$relations = new Relations(
+	$relations = new Hofff\Contao\LanguageRelations\Relations(
 		'tl_hofff_language_relations_news',
 		'hofff_language_relations_news_item',
 		'hofff_language_relations_news_relation'
 	);
 
-	$config = new RelationsDCABuilderConfig;
+	$config = new Hofff\Contao\LanguageRelations\DCA\RelationsDCABuilderConfig;
 	$config->setRelations($relations);
 	$config->setAggregateFieldName('pid');
 	$config->setAggregateView('hofff_language_relations_news_aggregate');
 	$config->setTreeView('hofff_language_relations_news_tree');
-	$config->setSelectriDataFactoryConfiguratorCallback(function(SQLAdjacencyTreeDataFactory $factory) {
-		$factory->getConfig()->addColumns([ 'date', 'type' ]);
-		$factory->getConfig()->setOrderByExpr('date DESC, title');
-	});
+	$config->setSelectriDataFactoryConfiguratorCallback(
+		function(Hofff\Contao\Selectri\Model\Tree\SQLAdjacencyTreeDataFactory $factory) {
+			$factory->getConfig()->addColumns([ 'date', 'type' ]);
+			$factory->getConfig()->setOrderByExpr('date DESC, title');
+		}
+	);
 	$config->setSelectriNodeLabelTemplate('hofff_language_relations_news_node_label');
 
-	$builder = new RelationsDCABuilder($config);
+	$builder = new Hofff\Contao\LanguageRelations\DCA\RelationsDCABuilder($config);
 	$builder->build($GLOBALS['TL_DCA']['tl_news']);
 });
